502282b631fe359be8cde96859f93cb019ce41e1
NathalieWK <54872220+NathalieWK@users.noreply.github.com>
2019-09-28 23:08:14 -0300

    Update app.673db9f7.js

--- app.673db9f7.js
+++ app.673db9f7.js
@@ -16269,9 +16269,9 @@
                         className: "version",
                         onClick: function()
                         {
-                            window.open("https://playdauntless.com/patch-notes/ob-0-9-3", "_blank"), ga("send", "event", "Version", "Version", "Version")
+                            window.open("https://playdauntless.com/patch-notes/1-0-0/", "_blank"), ga("send", "event", "Version", "Version", "Version")
                         }
-                    }, "OB 0.9.3"), r.a.createElement("div",
+                    }, "1.0.0"), r.a.createElement("div",
                     {
                         className: "privacy",
                         onClick: function()
